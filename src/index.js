bb60c42049d7570a8f47a9f180be16f897b843b4
Volodymyr <vovdata@gmail.com>
2022-11-17 17:45:45 +0100

    HA 10
    2 commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './css/styles.css';
-
+const refs = {
+    sb: document.querySelector('#search-box'),
+    cList: document.querySelector('.country-list'),
+    cInfo: document.querySelector('.country-info')
+}
+console.log(refs.sb);
+console.log(refs.cList);
+console.log(refs.cInfo);
 
 const DEBOUNCE_DELAY = 300;
