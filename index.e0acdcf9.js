d6f6764e6e44b098420abf900d8a4f7b214bea68
Vladimir-Plotnikov <71009993+Vladimir-Plotnikov@users.noreply.github.com>
2022-11-17 16:46:40 +0000

    Deploying to gh-pages from @ Vladimir-Plotnikov/goit-js-hw-10@bb60c42049d7570a8f47a9f180be16f897b843b4 🚀

---
+++ index.e0acdcf9.js
@@ -0,0 +1,2 @@
+!function(){var o={sb:document.querySelector("#search-box"),cList:document.querySelector(".country-list"),cInfo:document.querySelector(".country-info")};console.log(o.sb),console.log(o.cList),console.log(o.cInfo)}();
+//# sourceMappingURL=index.e0acdcf9.js.map
