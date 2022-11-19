0e0bf049f83752a1bdb134cff7a1202457be28ce
Volodymyr <vovdata@gmail.com>
2022-11-19 09:11:23 +0100

    HA 10
    new commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import './css/styles.css';
-const refs = {
-    sb: document.querySelector('#search-box'),
+let refs = {
+    sBox: document.querySelector('#search-box'),
     cList: document.querySelector('.country-list'),
     cInfo: document.querySelector('.country-info')
 }
-console.log(refs.sb);
-console.log(refs.cList);
 console.log(refs.cInfo);
+
+refs.sBox.addEventListener('input', onSearch)
+
+function onSearch(e){
+    e.preventDefault();
+    fetchCountries(e.target.value)
+}
+
+
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}`).then((response) => {
+        if (!response.ok) {
+            // throw new Error(response.status)
+            console.log('error');
+        }
+        return response.json();
+    }).then((data) => {
+        // console.log(data[0]);
+        console.log(data[0].flag);
+        console.log(data[0].altSpellings[2]);
+        console.log(data[0].capital[0]);
+        console.log(data[0].population);
+        console.log(data[0].languages);
+    })
+}
+
+
+
 
 const DEBOUNCE_DELAY = 300;
