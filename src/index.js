49d4e19ffb02558b4ee12e1fedb4d2bbb2759a1a
Volodymyr <vovdata@gmail.com>
2022-11-24 07:16:12 +0100

    HA 10
    last commit

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ let refs = {
     cList: document.querySelector('.country-list'),
     cInfo: document.querySelector('.country-info')
 }
+const debounce = require('lodash.debounce');
+const DEBOUNCE_DELAY = 300;
 // console.log(refs.cInfo);
-
-refs.sBox.addEventListener('input', onSearch)
-
-function onSearch(e){
+refs.sBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
+function onSearch(e) {
+    console.dir(e.path[0]);
     e.preventDefault();
+    if (e.target.value ==='') {
+        refs.cList.innerHTML = '';
+        refs.cInfo.innerHTML = '';
+    }
     fetchCountries(e.target.value)
 }
-
-
-const DEBOUNCE_DELAY = 300;
